279047c89b9748eef5fe35f46b3e98d49b82db8f
1000karat <92o3o92562@gmail.com>
2022-04-22 22:31:49 +0300

    add ternar.operator

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,16 @@
 public class Main {
     public static void main(String[] args) {
         int balace = 100; // На счёте сейчас
-        int replAmount = 1100; // Сумма пополнения счёта
-        int summBalance = balace + replAmount; // Сумма счёта без бонусов
+        int payment = 1100; // Сумма пополнения счёта
+        int summBalance = balace + payment; // Сумма счёта без бонусов
         int bonus; // Бонус
 
-        if (replAmount > 1000) {
-            bonus = replAmount / 100;
+        if (payment > 1000) {
+            bonus = payment / 100;
         } else {
             bonus = 0;
         }
+//        int bonus = payment > 1000 ? payment / 100 : 0;
 
         int total = summBalance + bonus; // Итоговая сумма счёта
 
