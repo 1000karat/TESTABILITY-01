5a15c90baa99bc59d9be5dc5f22828685aa0d4f2
1000karat <92o3o92562@gmail.com>
2022-04-26 21:35:57 +0300

    Create project

--- src/Main.java
+++ src/Main.java
@@ -1,22 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        int balace = 100; // На счёте сейчас
-        int payment = 1100; // Сумма пополнения счёта
-        int summBalance = balace + payment; // Сумма счёта без бонусов
-        int bonus; // Бонус
-
-        if (payment > 1000) {
-            bonus = payment / 100;
-        } else {
-            bonus = 0;
-        }
-//        int bonus = payment > 1000 ? payment / 100 : 0;
-
-        int total = summBalance + bonus; // Итоговая сумма счёта
-
-        System.out.println("Сумма без бонусов: " + summBalance + " рублей");
-        System.out.println("Количество бонусных рублей: " + bonus);
-        System.out.println("Итоговая сумма на счету: " + total + " рублей");
-
+        BonusMilesService service = new BonusMilesService();
+        int price = 10_000;
+        int miles = service.calculate(price);
+        System.out.println(miles);
     }
 }
